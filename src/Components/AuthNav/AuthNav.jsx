c13f41205c67056e029ee3e152531f2c98447b61
SupaFront <bluewotka@gmail.com>
2022-01-18 16:36:37 +0200

    123

--- src/Components/AuthNav/AuthNav.jsx
+++ src/Components/AuthNav/AuthNav.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getIsLoggedIn } from "../../redux/auth/authSelectors";
+import s from "./AuthNav.module.css";
 
 export default function AuthNav() {
   const isLoggedIn = useSelector(getIsLoggedIn);
@@ -10,7 +11,9 @@ export default function AuthNav() {
       {isLoggedIn ? (
         <NavLink to="/login">Log In</NavLink>
       ) : (
-        <NavLink to="/register">Sign Up</NavLink>
+        <NavLink className={s.pizz} to="/register">
+          Sign Up
+        </NavLink>
       )}
     </>
   );
